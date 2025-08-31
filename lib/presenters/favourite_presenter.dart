2dcea4c5c44b951fc4a30dbc1f1255f78d5ec0c0
DUDU0123 <sdu200115@gmail.com>
2025-08-31 20:26:14 +0530

    updated fav shared prefs issue

--- lib/presenters/favourite_presenter.dart
+++ lib/presenters/favourite_presenter.dart
@@ -2,19 +2,28 @@ import 'package:evide_bus_stop_app/app_utils/app_common_imports.dart';
 import 'package:evide_bus_stop_app/services/shared_prefs_services.dart';
 
 class FavouritePresenter extends GetxController{
+  @override
+  void onInit() {
+    super.onInit();
+    _loadFavorites();
+  }
+
+  Future<void> _loadFavorites() async {
+    final savedFavorites = await FavoritesManager.loadFavorites();
+    favoriteStops.value = savedFavorites;
+  }
 
   RxList<String> favoriteStops = <String>[].obs;
 
-  void addFavorite(String stopName) {
+  Future<void> addFavorite(String stopName) async {
     if (!favoriteStops.contains(stopName)) {
       favoriteStops.add(stopName);
       favSnackBar(message: 'Added to favorites');
-      FavoritesManager.saveFavorites(favoriteStops);
     }else{
       favoriteStops.remove(stopName);
       favSnackBar(message: 'Removed from favorites');
-      FavoritesManager.saveFavorites(favoriteStops);
     }
+    await FavoritesManager.saveFavorites(favoriteStops.toList());
   }
 
   favSnackBar({required String message}){
